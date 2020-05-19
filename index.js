39e756000a41e6c7e96ca49ae6cf9eaa391fa02f
Khuong Tran <dbcgdhh@gmail.com>
2020-05-19 10:21:12 +0700

    Echo PR title, change input variables

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const contextPullRequest = github.context.payload.pull_request;
+  if (!contextPullRequest) {
+    throw new Error(
+      "This action can only be invoked in `pull_request` events. Otherwise the pull request can't be inferred."
+    );
+  }
+
+  const prTitle = contextPullRequest.title;
+  console.log(prTitle);
 } catch (error) {
   core.setFailed(error.message);
 }
