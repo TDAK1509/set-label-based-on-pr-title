92ef6daa690c606295c901418b429cee91407c2e
Khuong Tran <dbcgdhh@gmail.com>
2020-05-19 10:51:17 +0700

    Test adding pr label

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ try {
   }
 
   const prTitle = contextPullRequest.title;
-  console.log(prTitle);
+  const prNumber = contextPullRequest.number;
+
+  const patternToCheck = core.getInput("pattern");
+  const label = core.getInput("label");
+
+  const repoToken = core.getInput("repo-token");
+  const octokit = new github.GitHub(repoToken);
+
+  octokit.issues
+    .addLabels({
+      ...github.context.repo,
+      number: prNumber,
+      labels: label,
+    })
+    .then((result) => {
+      console.log(result);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
